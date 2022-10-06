4b1c8be49506c52656faea7b0c89b38385b660af
jprice <jprice@izeni.com>
2022-10-06 08:41:09 -0600

    Use relay for users app queries

--- backend/apps/users/queries.py
+++ backend/apps/users/queries.py
@@ -1,23 +1,15 @@
 import graphene
 
+from graphene_django.filter import DjangoFilterConnectionField
+
 from .exceptions import PermissionDenied
 from .models import EmailUser
 from .types import EmailUserType
 
 
 class Query(graphene.ObjectType):
-    users = graphene.List(EmailUserType)
-    user = graphene.Field(EmailUserType, id=graphene.ID(required=True))
     me = graphene.Field(EmailUserType)
-
-    def resolve_users(root, info):
-        return EmailUser.objects.all()
-
-    def resolve_user(root, info, id):
-        try:
-            return EmailUser.objects.get(id=id)
-        except EmailUser.DoesNotExist:
-            return None
+    users = DjangoFilterConnectionField(EmailUserType)
 
     def resolve_me(root, info):
         if not info.context.user.is_authenticated:
--- backend/apps/users/types.py
+++ backend/apps/users/types.py
@@ -1,9 +1,13 @@
+import graphene
+from graphene import relay
 from graphene_django import DjangoObjectType
 
 from .models import EmailUser
 
 
 class EmailUserType(DjangoObjectType):
+    pk = graphene.ID(source="pk")
+
     class Meta:
         model = EmailUser
         exclude = (
@@ -11,3 +15,5 @@ class EmailUserType(DjangoObjectType):
             "verification_key",
             "reset_password_key",
         )
+        filter_fields = ("is_active",)
+        interfaces = (relay.Node,)
