1f2c2364618ce86737122c4c4a7b78c959a600bb
jprice <jprice@izeni.com>
2022-10-06 08:40:46 -0600

    Use relay for issues app queries

--- backend/apps/issues/queries.py
+++ backend/apps/issues/queries.py
@@ -1,50 +1,17 @@
 import graphene
 
-from .models import (
-    Project,
-    Label,
-    Issue,
-)
+from graphene import relay
+from graphene_django.filter import DjangoFilterConnectionField
+
 from .types import (
     ProjectType,
-    LabelType,
     IssueType,
 )
 
 
 class Query(graphene.ObjectType):
-    projects = graphene.List(ProjectType)
-    project = graphene.Field(ProjectType, id=graphene.ID(required=True))
+    projects = DjangoFilterConnectionField(ProjectType)
+    project = relay.Node.Field(ProjectType)
 
-    labels = graphene.List(LabelType)
-    label = graphene.Field(LabelType, id=graphene.ID(required=True))
-
-    issues = graphene.List(IssueType)
-    issue = graphene.Field(IssueType, id=graphene.ID(required=True))
-
-    def resolve_projects(root, info):
-        return Project.objects.all()
-
-    def resolve_project(root, info, id):
-        try:
-            return Project.objects.get(id=id)
-        except Project.DoesNotExist:
-            raise Project.DoesNotExist("Project does not exist")
-
-    def resolve_labels(root, info):
-        return Label.objects.all()
-
-    def resolve_label(root, info, id):
-        try:
-            return Label.objects.get(id=id)
-        except Label.DoesNotExist:
-            raise Project.DoesNotExist("Label does not exist")
-
-    def resolve_issues(root, info):
-        return Issue.objects.all()
-
-    def resolve_issue(root, info, id):
-        try:
-            return Issue.objects.get(id=id)
-        except Issue.DoesNotExist:
-            raise Project.DoesNotExist("Issue does not exist")
+    issues = DjangoFilterConnectionField(IssueType)
+    issue = relay.Node.Field(IssueType)
--- backend/apps/issues/types.py
+++ backend/apps/issues/types.py
@@ -1,3 +1,6 @@
+import graphene
+from django.db.models import Q
+from graphene import relay
 from graphene_django import DjangoObjectType
 
 from .models import (
@@ -8,10 +11,13 @@ from .models import (
 
 
 class ProjectType(DjangoObjectType):
+    pk = graphene.ID(source="pk")
+
     class Meta:
         model = Project
         fields = (
             "id",
+            "pk",
             "created_at",
             "modified_at",
             "name",
@@ -22,26 +28,45 @@ class ProjectType(DjangoObjectType):
             "labels",
             "issues",
         )
+        filter_fields = ("name",)
+        interfaces = (relay.Node,)
+
+    @classmethod
+    def get_queryset(self, queryset, info):
+        user = info.context.user
+        if not user.is_authenticated:
+            raise Exception("Permission denied")
+        if user.is_superuser:
+            return queryset.all()
+        return queryset.filter(Q(owner__id=user.id) | Q(members__id__in=[user.id]))
 
 
 class LabelType(DjangoObjectType):
+    pk = graphene.ID(source="pk")
+
     class Meta:
         model = Label
         fields = (
             "id",
+            "pk",
             "created_at",
             "modified_at",
             "project",
             "name",
             "color",
         )
+        filter_fields = ("project",)
+        interfaces = (relay.Node,)
 
 
 class IssueType(DjangoObjectType):
+    pk = graphene.ID(source="pk")
+
     class Meta:
         model = Issue
         fields = (
             "id",
+            "pk",
             "created_at",
             "modified_at",
             "project",
@@ -51,4 +76,24 @@ class IssueType(DjangoObjectType):
             "assignee",
             "labels",
             "due_date",
+            "closed_at",
+            "closed_by",
+        )
+        filter_fields = (
+            "project",
+            "title",
+            "assignee",
+            "due_date",
+        )
+        interfaces = (relay.Node,)
+
+    @classmethod
+    def get_queryset(self, queryset, info):
+        user = info.context.user
+        if not user.is_authenticated:
+            raise Exception("Permission denied")
+        if user.is_superuser:
+            return queryset.all()
+        return queryset.filter(
+            Q(project__owner__id=user.id) | Q(project__members__id__in=[user.id])
         )
